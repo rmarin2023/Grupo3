17a8f8a93409008397811e02c32278072614de12
clanchoc02 Lancho <clanchoc02@educarex.es>
2024-06-06 11:48:35 +0000

    Ej1

---
+++ presentación.java
@@ -0,0 +1,10 @@
+
+    /* HolaMundo.java
+ */
+
+presentación de clase pública
+{
+	público estático vacío principal (String [] argumentos) {
+		System.out.println("Hola mi nombre es Carlos Lancho Cuadrado!");
+	}
+}
