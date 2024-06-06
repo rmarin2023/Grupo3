e7f3b3bc386b2e9257ee27b3077e776f4cc4e878
clanchoc02 <clanchoc02@educarex.es>
2024-06-06 12:04:21 +0000

    Ej1 Carlos

--- presentacion.java
+++ presentacion.java
@@ -5,5 +5,8 @@ public class presentacion
 {
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es Marouan Messaaf Machrat!");
+
+		System.out.println("Hola mi nombre es Carlos Lancho Cuadrado!");
+
 	}
 }
